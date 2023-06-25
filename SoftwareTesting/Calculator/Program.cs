20195685ab3c9add57cc7bdb3deb405cc1da417e
grn-dev <mahmoudsabzalii@gmailcom>
2023-06-25 19:06:32 +0330

    feat : add test

--- SoftwareTesting/Calculator/Program.cs
+++ SoftwareTesting/Calculator/Program.cs
@@ -1,10 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 Console.WriteLine("------------------------");
 Calculator calculator = new();
 
 var sum = calculator.Sum(4, 5);
 Console.WriteLine("Sum is : " + sum);
 
-var divide = calculator.Divide(5, 5);
+var divide = calculator.Divide(5, 0);
 Console.WriteLine("Sum is : " + divide);
--- SoftwareTesting/CalculatorTest/UnitTest1.cs
+++ SoftwareTesting/CalculatorTest/UnitTest1.cs
@@ -22,4 +22,17 @@ public class Tests
         //assert
         Assert.AreEqual(9, sum);
     }
+
+    [Fact]
+    public void Divide()
+    {
+        //arrange
+        Calculator calculator = new();
+
+        //act
+        var result = calculator.Divide(10, 5);
+
+        //assert
+        Assert.AreEqual((2, 0), result);
+    }
 }
