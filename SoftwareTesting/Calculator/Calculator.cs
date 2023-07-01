e7e60276d2895d55c103758be9317f87e0975d19
grn-dev <mahmoudsabzalii@gmailcom>
2023-07-01 21:37:50 +0330

    test : static class

--- SoftwareTesting/Calculator/Calculator.cs
+++ SoftwareTesting/Calculator/Calculator.cs
@@ -1,9 +1,11 @@
-﻿public class Calculator
+﻿using System.Dynamic;
+
+public class Calculator
 {
     public int Sum(int first, int second)
     {
         return first + second;
-    } 
+    }
 
     public (int resutl, int remain) Divide(int first, int second)
     {
@@ -20,5 +22,26 @@
     public int Subtract(int first, int second)
     {
         return first - second;
+    }
+}
+
+public static class CalculatorExtension
+{
+    public static int PulseFourExtension(this int a)
+    {
+        return a + 4;
+    }
+}
+
+public interface IMoqClassWrapper
+{
+    int FourPlus(int a);
+}
+
+public class MoqClassWrapper : IMoqClassWrapper
+{
+    public int FourPlus(int a)
+    {
+        return a.PulseFourExtension();
     }
 }
