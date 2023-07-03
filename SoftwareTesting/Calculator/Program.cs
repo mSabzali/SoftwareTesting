b40440bb7b676f181f3e8a3bf817e42098c425e0
grn-dev <mahmoudsabzalii@gmailcom>
2023-07-03 23:17:11 +0330

    Extension

--- SoftwareTesting/Calculator/Program.cs
+++ SoftwareTesting/Calculator/Program.cs
@@ -1,9 +1,13 @@
-﻿
-Console.WriteLine("------------------------");
+﻿Console.WriteLine("------------------------");
 Calculator calculator = new();
 
 var sum = calculator.Sum(4, 5);
 Console.WriteLine("Sum is : " + sum);
+
+var time = DateTime.Now;
+
+int first= 80; 
+Console.WriteLine(first.PulseFourExtension());
 
 var divide = calculator.Divide(5, 0);
 Console.WriteLine("Sum is : " + divide);
